61cabee103fb4364adeef8ff99ac76756bf83e5e
Dibya Ranjan Barwa <dibya172000@yahoo.com>
2021-12-29 18:11:48 +0530

    SplashScreen

---
+++ app/src/main/java/com/example/news17/Splashscreen.kt
@@ -0,0 +1,18 @@
+package com.example.news17
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class Splashscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+
+        }, 5000)
+    }
+}
